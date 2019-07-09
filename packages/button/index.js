73c3c65f40143626ade16cefedf5efdff3ba809c
zhongsink <zhongink@gmail.com>
2019-07-09 23:25:16 +0800

    btn

--- packages/button/index.js
+++ packages/button/index.js
@@ -2,12 +2,6 @@ import './style.css'
 
 export default {
   name: 'ink-btn',
-  data() {
-    return {
-      active: false,
-      cacheStyle: {}
-    }
-  },
   computed: {
     typeClass() {
       return this.type === 'primary'
@@ -17,24 +11,6 @@ export default {
         : this.type === 'warning'
         ? 'btn-warning'
         : 'btn-default'
-    },
-    activeStyle() {
-      let style = {}
-      if (this.active) {
-        const btnStyles = window.getComputedStyle(this.$el)
-
-        Object.keys(this.actionStyle).forEach((el, index) => {
-          if (btnStyles[el]) {
-            this.cacheStyle[el] = btnStyles[el]
-            style = this.actionStyle[el]
-          }
-        })
-      } else {
-        Object.keys(this.cacheStyle).forEach((el, index) => {
-          style = this.cacheStyle[el]
-        })
-      }
-      return style
     },
     sizeClass() {
       if (this.size === 'large') return 'btn-large'
@@ -49,11 +25,9 @@ export default {
       type: String,
       default: 'small'
     },
-    actionStyle: {
-      type: Object,
-      default: () => {
-        return { background: '#f7f8f9' }
-      }
+    disabled: {
+      type: Boolean,
+      default: false
     }
   },
   render(h) {
@@ -64,14 +38,16 @@ export default {
           name: 'button',
           type: 'button'
         },
-        class: ['ink-btn', this.typeClass, this.sizeClass],
-        style: this.activeStyle,
+        class: [
+          'ink-btn',
+          this.typeClass,
+          this.sizeClass,
+          this.disabled ? 'is-disabled' : ''
+        ],
+        disabled: this.disabled,
         on: {
-          touchstart: () => {
-            this.active = true
-          },
-          touchend: () => {
-            this.active = false
+          click: evt => {
+            this.$emit('click', evt)
           }
         }
       },
